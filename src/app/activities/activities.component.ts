93d19d2e17fb05c44138972e9639e41c7907c8a5
unknown <ramkrish1214@gmail.com>
2020-09-19 18:01:17 +0530

    upgrading angular 4 to angular 10 - rxjs updates

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -1,4 +1,14 @@
+<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
+=======
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { CompletedActivities } from './completed-activities/completed-activities.model';
+import { CompletedActService } from './completed-activities/completed-act.service';
+import { PendingActivities } from './pending-activities/pending-activities.model';
+import { PendingActService } from './pending-activities/pending-act.service';
+>>>>>>> 8d700c5... upgrading angular 4 to angular 10 - rxjs updates
 
 @Component({
   selector: 'app-activities',
--- src/app/activities/pending-activities/pending-act.service.ts
+++ src/app/activities/pending-activities/pending-act.service.ts
@@ -1,5 +1,5 @@
 import { EventEmitter } from '@angular/core'
-import { Subject } from 'rxjs/Subject';
+import { Subject } from 'rxjs';
 
 import { PendingActivities } from './pending-activities.model';
 
